917c76917d9f270d690437de1304172fa58ae2b5
caoxiang18 <145153824+caoxiang18@users.noreply.github.com>
2025-08-06 16:56:57 +0800

    Update code1.cpp

--- 9function/code1.cpp
+++ 9function/code1.cpp
@@ -1,1 +1,17 @@
+#include<iostream>
+ 
+int Multiply(int a, int b) {
+	return a * b;
+}
 
+int main() {
+	int result1 = Multiply(3, 2);
+	std::cout << result1 << std::endl;
+
+	int result2 = Multiply(8, 5);
+	std::cout << result2 << std::endl;
+
+	int result3 = Multiply(90, 20);
+	std::cout << result3 << std::endl;
+	std::cin.get();
+}
